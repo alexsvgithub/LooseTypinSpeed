f07608086e955a710834bb5bd7e721b9b92b306e
alexsvgithub <53620960+alexsvgithub@users.noreply.github.com>
2020-10-01 10:46:58 +0530

    add seperate js file

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+  setInterval(function () {document.getElementById("btn").click();}, 60000);
+  function fun(){
+
+
+    let a=document.getElementById('nn').value;
+    var leng = (a.split(" ").length );
+
+      document.open();
+      document.write("<h1>speed is "+leng+" per minute sec</h1>");
+      document.close();
+
+  }
